4cc6e59956eeb4d72336e535043cd4f342be70d3
photondragon <photondragon@163.com>
2016-05-28 00:58:24 +0800

    Initial Commit

---
+++ IDNWxPayerSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IDNWxPayerSample
+//
+//  Created by photondragon on 16/5/28.
+//  Copyright © 2016年 photondragon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
